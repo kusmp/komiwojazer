529d3cf7e58d3bc677dcf1c8d3b49a1f04a4d44f
kusmp <patryk.kusmierkiewicz@gmail.com>
2019-01-19 17:47:59 +0100

    fix GreedyCycle

--- src/main/java/algorithms/initial_division/GreedyCycle.java
+++ src/main/java/algorithms/initial_division/GreedyCycle.java
@@ -8,13 +8,19 @@ import java.util.List;
 import java.util.Map;
 
 public class GreedyCycle implements DivisionAlgorithm {
-    public GreedyCycle() {}
+
+    private final City startCity;
+
+    public GreedyCycle(City startCity){
+        this.startCity = startCity;
+    }
+
 
     @Override
     public City nextPoint(List<City> points, City actualPoint) {
         Map<City, Double> distanceLengths = new HashMap<>();
         for (City point: points) {
-            double summaryDistance = EuclideanDistance.calc(actualPoint, point) + EuclideanDistance.calc(points.get(0), point);
+            double summaryDistance = EuclideanDistance.calc(actualPoint, point) + EuclideanDistance.calc(startCity, point);
             distanceLengths.put(point, summaryDistance);
         }
         Map.Entry<City, Double> min = null;
